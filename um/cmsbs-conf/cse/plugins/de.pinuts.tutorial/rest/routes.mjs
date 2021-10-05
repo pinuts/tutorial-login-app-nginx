6c4a3a472cd8d15fab570bd62b17a751de3c3b99
Florian Wolff <florian.wolff@pinuts.de>
2021-10-05 14:20:35 +0200

    console log

--- um/cmsbs-conf/cse/plugins/de.pinuts.tutorial/rest/routes.mjs
+++ um/cmsbs-conf/cse/plugins/de.pinuts.tutorial/rest/routes.mjs
@@ -7,13 +7,21 @@ import { RouterBuilder } from "@de.pinuts.apirouter/shared/routing.es6";
  * @param {HttpResponse} res 
  */
 const auth = (req, res) => {
-    const originalUri = req.header('x-original-uri');
+    const originalUri = new java.net.URI(req.header('x-original-uri'));
+
+    /** @type {Entry} */
     const entry = new de.pinuts.cmsbs.lib.plugin.SessionHandler().getUserEntry();
 
     if (!entry) {
         return res.sendError(401);
     } else {
-        console.log(`/auth: "${originalUri}"; headers =`, req.headers, req.session, entry);
+        console.log(`/auth:
+        originalUri.path=${originalUri.path}
+        entry.oid=${entry.oid}
+        req.headers=${JSON.stringify(req.headers)}
+        req.session=${JSON.stringify(req.session)}`);
+
+        // TODO: Implement appropriate checks against originalUri and entry...
     }
 }
 
