f89f07a4911df24d77560539665f333d95e3c4e8
Florian Wolff <florian.wolff@pinuts.de>
2021-10-05 14:12:42 +0200

    Check User Session

--- um/cmsbs-conf/cse/plugins/de.pinuts.tutorial/rest/routes.mjs
+++ um/cmsbs-conf/cse/plugins/de.pinuts.tutorial/rest/routes.mjs
@@ -8,8 +8,13 @@ import { RouterBuilder } from "@de.pinuts.apirouter/shared/routing.es6";
  */
 const auth = (req, res) => {
     const originalUri = req.header('x-original-uri');
-    console.log(`/auth: "${originalUri}"; headers =`, req.headers, req.session);
-    // res.status = 401;
+    const entry = new de.pinuts.cmsbs.lib.plugin.SessionHandler().getUserEntry();
+
+    if (!entry) {
+        return res.sendError(401);
+    } else {
+        console.log(`/auth: "${originalUri}"; headers =`, req.headers, req.session, entry);
+    }
 }
 
 de.pinuts.tutorial.apiController = new RouterBuilder()
